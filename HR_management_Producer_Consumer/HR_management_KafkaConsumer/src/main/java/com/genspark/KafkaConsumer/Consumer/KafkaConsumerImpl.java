087554c6d56f14fb1142d696a34c86c0a40be4ed
hana <hanane7885@gmail.com>
2023-03-30 13:28:05 -0500

    consumer write to a log file

--- HR_management_Producer_Consumer/HR_management_KafkaConsumer/src/main/java/com/genspark/KafkaConsumer/Consumer/KafkaConsumerImpl.java
+++ HR_management_Producer_Consumer/HR_management_KafkaConsumer/src/main/java/com/genspark/KafkaConsumer/Consumer/KafkaConsumerImpl.java
@@ -3,6 +3,9 @@ package com.genspark.KafkaConsumer.Consumer;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
+import java.io.File;
+import java.io.FileWriter;
+
 @Service
 public class KafkaConsumerImpl implements KafkaConsumerInt{
 	@KafkaListener(topics = "HrEventTopic", groupId = "Group1")
@@ -10,5 +13,19 @@ public class KafkaConsumerImpl implements KafkaConsumerInt{
 	{
 
 		System.out.println("Received '" + hrEvent +"' from the HrEventTopic." );
+
+		FileWriter out ;//= null;
+		String output="HR_Activity_log.txt";
+		File file=new File(output);
+		try {
+			out = new FileWriter(file);
+			out.write("event");
+			//out.write("event : ");
+			out.write(hrEvent);
+			System.out.println("writing in a HR_Activity_log file!!!");
+		}
+		catch(Exception e) {
+			System.out.println(e);
+		}
 	}
 }
